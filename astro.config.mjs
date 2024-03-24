632bc953ccb676449694c6f95df0ed699f1d4f06
callYandex <112511246+callYandex@users.noreply.github.com>
2024-03-24 20:40:05 +0100

    test xii

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
     site: 'https://chemflummi.github.io',
-    base: 'ACMods',
     integrations: [mdx(), sitemap(), tailwind()]
   });
